619ceed43c78705eb6516db38b7125f578e02359
Andrew Zimmerman <astevezimm@gmail.com>
2022-08-01 21:43:32 -0600

    minor tweaks

--- components/Layout.js
+++ components/Layout.js
@@ -26,7 +26,7 @@ export default function Layout(props) {
                     <a href="https://github.com/astevezimm/potato-salad" target="_blank">Source code for this site available here!</a>
                 </h4>
                 <h4>
-                    <a href="https://andrew-zimmerman.herokuapp.com/" target="_blank">And here's my main website</a>
+                    <a href="https://andrew-zimmerman.herokuapp.com/" target="_blank">And here's my main website!</a>
                 </h4>
                 <p>Andrew Zimmerman Copyright © {new Date().getFullYear()}</p>
             </footer>
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,3 @@
 export default function HomePage() {
-    return <h1>Making Potato Salad</h1>
+    return <h1>Home Page</h1>
 }
