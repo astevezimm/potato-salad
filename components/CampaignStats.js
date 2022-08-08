333acf54a018505059baf0c27725463d7506b7c1
Andrew Zimmerman <astevezimm@gmail.com>
2022-08-08 15:27:04 -0600

    styling on stats & start of percent bar

--- components/CampaignStats.js
+++ components/CampaignStats.js
@@ -4,11 +4,17 @@ export default function CampaignStats() {
     return (
         <div className={classes.stats}>
             <div className={classes.stat}>
-                <h2>$10,500<br />raised of<br />$1,000,000<br />%1</h2>
+                <h2><span className={classes.amount}><i>$</i>10,500</span>
+                    <span className={classes["raised-of-label"]}>raised of</span>
+                    <span className={classes.goal}><i>$</i>1,000,000</span>
+                    <span className={classes.percent}>1<i>%</i> of goal</span>
+                </h2>
             </div>
             <div className={classes.stat}>
-                <h2>18<br />days left</h2>
+                <h2><span className={classes.day}>18</span>
+                    <span className={classes["days-left-label"]}>days left</span></h2>
             </div>
+            <div className={classes["percent-bar"]} />
         </div>
     );
 }
--- components/layout/TopSection.js
+++ components/layout/TopSection.js
@@ -3,7 +3,7 @@ import CampaignStats from "../CampaignStats";
 
 export default function TopSection(props) {
     return (
-      <div className="row">
+      <div className={"row " + classes.top}>
           <div className="col-xl-6 col-lg-8">
               <img className={classes.top} src={props.image} alt="potato salad" />
           </div>
