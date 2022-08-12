893fa99bb34bb78fb62da072ec6a6301dba2d496
Andrew Zimmerman <astevezimm@gmail.com>
2022-08-12 14:53:32 -0600

    add missing data and adjust styling

--- components/CampaignStats.js
+++ components/CampaignStats.js
@@ -2,17 +2,22 @@ import classes from "./CampaignStats.module.css"
 
 export default function CampaignStats() {
     return (
-        <div className={classes.stats}>
-            <div className={classes.stat}>
-                <h2><span className={classes.amount}><i>$</i>10,500</span>
-                    <span className={classes["raised-of-label"]}>raised of</span>
-                    <span className={classes.goal}><i>$</i>1,000,000</span>
-                    <span className={classes.percent}>1<i>%</i> of goal</span>
-                </h2>
-            </div>
-            <div className={classes.stat}>
-                <h2><span className={classes.day}>18</span>
-                    <span className={classes["days-left-label"]}>days left</span></h2>
+        <div className={classes["stats-area"]}>
+            <div className={classes.stats}>
+                <div className={classes.stat}>
+                    <h2><span className={classes.amount}><i>$</i>10,500</span>
+                        <span className={classes["raised-of-label"]}>raised of</span>
+                        <span className={classes.goal}><i>$</i>1,000,000</span>
+                        <span className={classes.percent}>1<i>%</i> of goal</span>
+                        <span className={classes.backers}>by 250 backers</span>
+                    </h2>
+                </div>
+                <div className={classes.stat}>
+                    <h2><span className={classes.day}>18</span>
+                        <span className={classes["days-left-label"]}>days left</span>
+                        <span className={classes["end-date"]}>ends Aug 22, 2022</span>
+                    </h2>
+                </div>
             </div>
             <div className={classes["percent-bar"]} />
         </div>
