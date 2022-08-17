19f2b73f7969d172037408772b3d778f8aff1aae
Andrew Zimmerman <astevezimm@gmail.com>
2022-08-17 11:41:01 -0600

    start implementation of graph

--- components/top-section/img-graph/Graph.js
+++ components/top-section/img-graph/Graph.js
@@ -3,6 +3,6 @@ import classes from "./Graph.module.css"
 export default function Graph() {
     console.log("Graph active");
     return (
-        <></>
+        <div className={classes.graph}></div>
     )
 }
