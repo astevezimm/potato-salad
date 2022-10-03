102946d419afda17bd5f2709ebd5f89edf8201c5
Andrew Zimmerman <astevezimm@gmail.com>
2022-10-03 16:49:22 -0600

    fix issue with graph

--- components/top-section/img-graph/Graph.js
+++ components/top-section/img-graph/Graph.js
@@ -2,17 +2,19 @@ import classes from "./Graph.module.css"
 import {useSelector} from "react-redux";
 
 export default function Graph() {
-    const {dailyAmounts} = useSelector(state => state.campaignStats);
-    const maxAmount = Math.max(dailyAmounts);
+    const {dailyAmounts, goal} = useSelector(state => state.campaignStats);
+    
+    let total = 0;
     
     function calcPercent(amount) {
-        return Math.min((amount / maxAmount) * 100, 100);    
+        total += amount;
+        return Math.min((total / goal) * 100, 100);    
     }
         
     return (
         <ul className={classes.graph}>
             {dailyAmounts.map((amount, index) => (
-                <li key={index} className={classes.bar} style={{height: calcPercent(amount)}} />
+                <li key={index} className={classes.bar} style={{height: `${calcPercent(amount)}%`}} />
             ))}
         </ul>
     )
